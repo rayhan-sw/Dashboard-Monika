6fc98325e802ca48ae2575d5fc6a5d5cdc6e3176
Moh Yusril Nur Syabani <yusrilnursabai12@gmail.com>
2026-02-23 11:58:36 +0700

    Clean Temporary File

--- scripts/editor-overwrite-msg.ps1
+++
@@ -1,4 +0,0 @@
-# Overwrite commit message file with clean message (no co-author)
-# Git passes the message file path as first argument
-$msgPath = $args[0]
-if ($msgPath) { Set-Content -Path $msgPath -Value "Update terbaru" }
